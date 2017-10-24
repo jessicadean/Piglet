3ce6dded34a9f60759d30cd81d542e8c9982b3a5
jessicadean <30277926+jessicadean@users.noreply.github.com>
2017-10-24 14:45:30 -0400

    Add project files.

---
+++ jdean-budgeter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jdean_budgeter.MvcApplication" Language="C#" %>
---
+++ jdean-budgeter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jdean_budgeter.Startup))]
+namespace jdean_budgeter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
